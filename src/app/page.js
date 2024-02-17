c979086946774173b93e61e1c8399f7f98f2faf6
MartinDevHub <elielmartinmarcoff@gmail.com>
2024-02-17 12:56:58 -0300

    created components

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,29 @@
-import Image from "next/image";
+import Welcome from "@/app/components/home/welcome";
+import About from "./components/home/about";
+import Projects from "./components/home/projects";
+import CV from "./components/home/cv";
 
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="flex flex-col gap-40 items-center">
+      <div className="h-screen flex items-center max-w-screen-xl">
+        <Welcome />
+      </div>
+      <div className="bg-gray-900 w-screen flex justify-center py-20 shadow-md">
+        <div className="w-full max-w-screen-xl">
+          <About />
+        </div>
+      </div>
+      <div className="bg-gray-900 w-screen flex justify-center py-20 shadow-md">
+        <div className="w-full max-w-screen-xl">
+          <Projects />
+        </div>
+      </div>
+      <div className="bg-gray-900 w-screen flex justify-center py-20 shadow-md">
+        <div className="w-full max-w-screen-xl">
+          <CV />
+        </div>
+      </div>
+    </div>
+  );
 }
