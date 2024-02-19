b6cd60c7203ab56eb877c2b5e6c662c7f9d0439a
MartinDevHub <elielmartinmarcoff@gmail.com>
2024-02-19 14:24:45 -0300

    new video added

--- src/app/components/home/welcome.jsx
+++ src/app/components/home/welcome.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 
 export default function Welcome() {
-  const videoUrl =
-    "https://www.youtube.com/embed/BadB1z-V_qU?autoplay=1&controls=0&modestbranding=1&showinfo=0&loop=1&mute=1";
-
   return (
     <div className="h-screen w-screen flex items-center justify-center">
       <video
@@ -13,7 +10,7 @@ export default function Welcome() {
         playsInline
         className="w-full h-full object-cover"
       >
-        <source src="/video.mp4" type="video/mp4" />
+        <source src="/welcome-video.mp4" type="video/mp4" />
         Tu navegador no soporta el elemento de video.
       </video>
       <h1 className="text-9xl font-bold text-white absolute z-10">
