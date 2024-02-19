ae66e0032eaba44b4ffdbc0ceb671a8c36bb6499
MartinDevHub <elielmartinmarcoff@gmail.com>
2024-02-19 15:48:37 -0300

    modified ui: put projects next to contact

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import Contact from "./components/home/contact";
 export default function Home() {
   return (
     <div className="flex flex-col items-center">
-      <div className="h-screen flex items-center ">
+      <div className="h-screen flex items-center">
         <Welcome />
       </div>
       <div className="bg-gray-900 w-screen flex justify-center py-5 shadow-md">
@@ -15,13 +15,13 @@ export default function Home() {
         </div>
       </div>
       <div className="bg-gray-900 w-screen flex justify-center py-5 shadow-md">
-        <div className="w-full max-w-screen-xl">
-          <Projects />
-        </div>
-      </div>
-      <div className="bg-gray-900 w-screen flex justify-center py-5 shadow-md">
-        <div className="w-full max-w-screen-xl">
-          <Contact />
+        <div className="w-full max-w-screen-xl flex flex-wrap justify-between">
+          <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2">
+            <Projects />
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2">
+            <Contact />
+          </div>
         </div>
       </div>
     </div>
