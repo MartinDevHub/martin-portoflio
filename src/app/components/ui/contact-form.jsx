5132caa0cb0699daecff8cee7485c77cfcb9af4a
MartinDevHub <elielmartinmarcoff@gmail.com>
2024-02-19 18:32:53 -0300

    basic form ui

--- src/app/components/ui/contact-form.jsx
+++ src/app/components/ui/contact-form.jsx
@@ -17,32 +17,48 @@ export default function ContactForm() {
       acceptCharset="utf-8"
       action="https://formspree.io/f/mleqbzyv"
       method="post"
+      className="max-w-md mx-auto"
     >
       <fieldset id="fs-frm-inputs">
-        <label htmlFor="full-name">Full Name</label>
-        <input
-          type="text"
-          name="name"
-          id="full-name"
-          placeholder="First and Last"
-          required
-        />
-        <label htmlFor="email-address">Email Address</label>
-        <input
-          type="email"
-          name="_replyto"
-          id="email-address"
-          placeholder="email@domain.tld"
-          required
-        />
-        <label htmlFor="message">Message</label>
-        <textarea
-          rows="5"
-          name="message"
-          id="message"
-          placeholder=""
-          required
-        />
+        <div className="mb-4">
+          <label htmlFor="full-name" className="block text-gray-600">
+            Full Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="full-name"
+            placeholder="First and Last Name"
+            required
+            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-400 text-gray-800"
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="email-address" className="block text-gray-600">
+            Email Address
+          </label>
+          <input
+            type="email"
+            name="_replyto"
+            id="email-address"
+            placeholder="Email"
+            required
+            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-400 text-gray-800"
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="message" className="block text-gray-600">
+            Message
+          </label>
+          <textarea
+            rows="5"
+            name="message"
+            id="message"
+            placeholder="Message"
+            required
+            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-400 text-gray-800"
+          />
+        </div>
         <input
           type="hidden"
           name="_subject"
@@ -50,7 +66,11 @@ export default function ContactForm() {
           value="Contact Form Submission"
         />
       </fieldset>
-      <input type="submit" value="Submit" />
+      <input
+        type="submit"
+        value="Submit"
+        className="w-full px-4 py-2  text-white rounded-md cursor-pointer"
+      />
     </form>
   );
 }
