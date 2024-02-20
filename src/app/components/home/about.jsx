d7ae62f2c7e508fb6dff6cdb206d19752a0137c3
MartinDevHub <elielmartinmarcoff@gmail.com>
2024-02-20 19:42:44 -0300

    subtitles: font bold

--- src/app/components/home/about.jsx
+++ src/app/components/home/about.jsx
@@ -4,7 +4,7 @@ import AboutImage from "@/app/components/ui/about-image";
 const About = () => {
   return (
     <div>
-      <div className="text-4xl pt-5 py-5">
+      <div className="text-4xl pt-5 py-5 font-bold">
         <h1>About Me</h1>
       </div>
       <AboutImage />
