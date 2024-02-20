78a94728e352a1023cd1c820fa1c442ec48bcc17
MartinDevHub <elielmartinmarcoff@gmail.com>
2024-02-20 19:49:12 -0300

    about: take out font bold from text

--- src/app/components/ui/about-image.jsx
+++ src/app/components/ui/about-image.jsx
@@ -24,9 +24,9 @@ const AboutImage = () => {
         objectFit="cover"
         style={{ filter: "grayscale(100%)", mixBlendMode: "multiply" }}
       />
-      <div className="absolute inset-0 flex items-start justify-start text-white  font-bold p-2 h-2/3 w-2/3">
+      <div className="absolute inset-0 flex items-start justify-start text-white  p-2 h-2/3 w-2/3">
         <div>
-          <div className=" font-bold p-2 text-2xl">
+          <div className="p-2 text-2xl">
             Fullstack software engineer. Degreed social scientist. Passionate
             about rationally understanding the &quot;why&quot; behind things.
             Constant learner. Enthusiastic teacher. Proud nerd.
