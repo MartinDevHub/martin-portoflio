b6dbdd3cb53bb64d46e1c7deb222c02bebb06281
MartinDevHub <elielmartinmarcoff@gmail.com>
2024-02-27 17:41:54 -0300

    implemented default button in about component + unified style

--- src/app/components/ui/default-button.jsx
+++ src/app/components/ui/default-button.jsx
@@ -1,8 +1,11 @@
 export default function DefaultButton({ text, url }) {
+  const buttonStyle =
+    "font-bold text-white bg-blue-900 hover:bg-gray-500 hover:text-blue-900 px-6 py-1 rounded-xl cursor-pointer text-sm sm:text-lg mt-3 mb-1";
+
   if (url) {
     return (
       <a
-        className="text-white bg-blue-900 text-sm sm:text-lg px-6 py-1 rounded-xl mt-3 sm:mb-1 mb-5"
+        className={buttonStyle}
         href={url}
         target="_blank"
         rel="noopener noreferrer"
@@ -13,9 +16,7 @@ export default function DefaultButton({ text, url }) {
   } else {
     return (
       <div>
-        <button className="text-white bg-blue-900 px-6 py-1 rounded-xl cursor-pointer text-sm sm:text-lg mt-3 mb-1">
-          {text}
-        </button>
+        <button className={buttonStyle}>{text}</button>
       </div>
     );
   }
